74d713f271ad999c32995f77b5c9ddc59c599db2
luffy <rainriver5014@gmail.com>
2021-08-31 07:13:45 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path')
+let app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
